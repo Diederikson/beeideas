5a47d583e6fafceb44cd5ef48742eaf44658e6a6
Diederik Oudshoorn <diederikson@gmail.com>
2020-03-16 13:28:11 +0100

    bijgewerkt

---
+++ Python/Codewars/_notes.md
@@ -0,0 +1,12 @@
+20200316 Takeaways van opdracht anagram
+
+- string.replace(x, 'r',1) geeft alleen het replace resultaat weer. De oorspronkelijke string blijft ongewijzigd
+- Toevoegen aan lists gaat met append
+- In de verzameling oplossingen staan vereenvoudigde exemplaren. Maar ja, ik ken nog niet alle hendigheidjes hè?
+
+20200316|13:05
+
+Klaar voor vandaag met Python
+
+ 
+
--- Python/Codewars/anagram.py
+++ Python/Codewars/anagram.py
@@ -1,1 +1,34 @@
-.
+#Anagrams
+#De input is een woord en een lijst met woorden.
+#de output is de selectie uit de lijst van woorden die een anagram van
+#het input woord zijn.
+#oplossingsrichting:
+#ik itereer over de letters van het woord
+#is de iter aanwezig in de lijst? streepweg:geenana!
+#zijn alle letters op?klaar:geenana!
+
+#DJO 20200316
+
+def anagrams(word, words):
+    result,ana,z =[],True,''
+    for y in words:
+        z=y
+        for x in word:
+            if x not in z:
+                ana=False
+                break
+            z=z.replace(x,'',1)
+        if len(z)>0:
+            ana=False
+        if ana==True:
+            result.append(y)
+        ana=True
+    return result
+
+w='racer'
+wds=['crazer', 'carer', 'racar', 'caers', 'racer']
+
+print(anagrams(w,wds))
+
+    
+    
