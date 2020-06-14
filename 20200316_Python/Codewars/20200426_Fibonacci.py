1c385e6dbc467f7e5e228e974b7a806656d1634a
Diederik Oudshoorn <diederikson@gmail.com>
2020-06-14 22:25:56 +0200

    Update 20200426_Fibonacci.py
    Bijna deez aanvullingen plijton.

--- 20200316_Python/Codewars/20200426_Fibonacci.py
+++ 20200316_Python/Codewars/20200426_Fibonacci.py
@@ -1,3 +1,5 @@
+"""
+YO! Triple Quotes is here to stay!
 #Memoizing Fibonacci | Codewars
 #---------------------------------------------------------------
 # Fibonacci functie is bronnen intensief, door recursie
@@ -23,16 +25,29 @@
 # end en sep parameters bij print. End geeft alternatief voor newline en
 # sep geeft aan wat er tussen de kommas komt (normaal een space)
 #---
+# En fuckme over. Een hele lap commentaar weer pleite. Wat een
+# enorm gekloot is dit toch. Ik heb de recursie uitgezocht gisteren
+# (en ben dus de aantekeningen kwijt). De belangrijkste bevinding
+# is dat de eerste functie van regel 40 net zolang wordt aange
+# roepen totdat die returnt. Dan komt de tweede functie pas in
+# in beeld. Ruckzooi. Menfin youptjoep het nog maar een keer
+# Er is overigens geen behoorlijk joeptjoep te vinden. Maarrr
+# Zoeken op animation Fibonacci recursion, levert een heel mooi
+# animation op alwel hier:
+# www.cs.usfca.edu/~galles/visualization/DPFib.html
+# ---
+# enfin ikke denk wij maak een dict met daarin de paren
+# (getal, fibno) als key value
+# Gekuh henkie. Dict gemaakt eerste twee waarden toegevoegd,
+# maar nu is de vraag hoe vul je die met een tussen resultaat?
 #
-#
+"""
 def fibonacci(n):
-    mem=[]
+    mem={0:0,1:1}
     def calc(n):
-        if n in [0,1]:
-            return n
         if n in mem:
             return mem[n]        
-        mem.append(n)
+        mem[n]=//Jahaaa hier ziet m de crux 
         print(mem)
         return calc(n - 1) + calc(n - 2)
     return calc(n)
